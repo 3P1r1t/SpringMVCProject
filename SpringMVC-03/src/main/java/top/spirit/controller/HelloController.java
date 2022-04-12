572c904864fcc1ff3060a1750bea144d19cf867b
spirit <1363037485@qq.com>
2022-04-12 16:46:51 +0800

    4/12提交

---
+++ SpringMVC-03/src/main/java/top/spirit/controller/HelloController.java
@@ -0,0 +1,20 @@
+package top.spirit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/h1")
+    public String hello1(Model model){
+        model.addAttribute("msg1","HelloSpringAnnotaction1");
+        return "hello1";
+    }
+    @RequestMapping("/h2")
+    public String hello2(Model model){
+        model.addAttribute("msg2","HelloSpringAnnotaction2");
+        return "hello2";
+    }
+}
+
